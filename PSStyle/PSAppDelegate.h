a972e464e0a91bfeff640457ed1a19b847a72930
Paul Samuels <p.samuels@mmu.ac.uk>
2012-09-11 18:02:03 +0100

    Initial Commit

---
+++ PSStyle/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  PSStyle
+//
+//  Created by Paul Samuels on 11/09/2012.
+//  Copyright (c) 2012 Paul Samuels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PSStyle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PSStyle
+//
+//  Created by Paul Samuels on 11/09/2012.
+//  Copyright (c) 2012 Paul Samuels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+  }
+}
